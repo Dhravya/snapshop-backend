48a0dac9a0a2a23056135966dce4a471643009ab
Dhravya <dhravyashah@gmail.com>
2023-11-12 08:49:17 -0500

    added don't die script

---
+++ script.py
@@ -0,0 +1,7 @@
+import requests
+from time import sleep
+
+while True:
+    response = requests.get('https://dropit2-production.up.railway.app/googleSearch?itemName=White Dress Shirt Women')
+    print(response.text)
+    sleep(10)
